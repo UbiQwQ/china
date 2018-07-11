e0be8023b420aaf3b349efa25e4929de1022ebda
liwt <a920526770+>
2018-07-11 14:13:11 +0800

    init

---
+++ src/main/java/blct/china/china/ChinaApplication.java
@@ -0,0 +1,12 @@
+package blct.china.china;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChinaApplication.class, args);
+	}
+}
---
+++ src/test/java/blct/china/china/ChinaApplicationTests.java
@@ -0,0 +1,16 @@
+package blct.china.china;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChinaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
