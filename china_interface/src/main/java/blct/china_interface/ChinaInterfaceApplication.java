aad6d2120434cf57bb7bb330a768bfce6f4bd3e5
liwt <a920526770+>
2018-07-11 20:05:38 +0800

    init

---
+++ china_interface/src/main/java/blct/china_interface/ChinaInterfaceApplication.java
@@ -0,0 +1,12 @@
+package blct.china_interface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChinaInterfaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChinaInterfaceApplication.class, args);
+	}
+}
---
+++ china_web/src/main/java/blct/china_web/ChinaWebApplication.java
@@ -0,0 +1,37 @@
+package blct.china_web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@SpringBootApplication
+public class ChinaWebApplication {
+
+
+    private static final Logger log = LoggerFactory.getLogger(ChinaWebApplication.class);
+
+    public static void main(String[] args) throws UnknownHostException {
+        ConfigurableApplicationContext content = SpringApplication.run(ChinaWebApplication.class, args);
+        ConfigurableEnvironment env = content.getEnvironment();
+        String protocol = "http";
+        if (env.getProperty("server.ssl.key-store") != null) {
+            protocol = "https";
+        }
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "Local: \t\t{}://localhost:{}\n\t" +
+                        "External: \t{}://{}:{}\n----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                protocol,
+                env.getProperty("server.port"),
+                protocol,
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port"));
+    }
+}
